06fc6a7177adb6610aed359acd8159fffab9cce6
Emanuel Claesson <emanuel.claesson@gmail.com>
2012-02-04 17:24:24 +0100

    Finished comment-viewing

--- gen/net/radspark/android/warpdrive/R.java
+++ gen/net/radspark/android/warpdrive/R.java
@@ -25,29 +25,33 @@ public final class R {
         public static final int warplogo_bg=0x7f020008;
     }
     public static final class id {
-        public static final int backItem=0x7f070015;
+        public static final int backItem=0x7f070019;
         public static final int browseList=0x7f070000;
-        public static final int commentList=0x7f070012;
-        public static final int forwardItem=0x7f070016;
-        public static final int mainLayout=0x7f070001;
-        public static final int mainMenuItem=0x7f070013;
-        public static final int mainMenuList=0x7f07000e;
-        public static final int progressBar1=0x7f07000b;
-        public static final int publishText=0x7f070010;
-        public static final int quoteCommentBubble=0x7f070007;
-        public static final int quoteComments=0x7f070008;
-        public static final int quoteFavStar=0x7f070005;
-        public static final int quoteFavorites=0x7f070006;
-        public static final int quoteFootnote=0x7f07000a;
-        public static final int quoteGrade=0x7f070004;
-        public static final int quoteId=0x7f070003;
-        public static final int quoteText=0x7f070009;
-        public static final int quoteTopBar=0x7f070002;
-        public static final int readersText=0x7f070011;
-        public static final int reportText=0x7f07000f;
-        public static final int textView1=0x7f07000c;
-        public static final int updateItem=0x7f070014;
-        public static final int warplogo=0x7f07000d;
+        public static final int commentAuthor=0x7f070002;
+        public static final int commentDate=0x7f070003;
+        public static final int commentList=0x7f070016;
+        public static final int commentText=0x7f070004;
+        public static final int forwardItem=0x7f07001a;
+        public static final int linearLayout1=0x7f070001;
+        public static final int mainLayout=0x7f070005;
+        public static final int mainMenuItem=0x7f070017;
+        public static final int mainMenuList=0x7f070012;
+        public static final int progressBar1=0x7f07000f;
+        public static final int publishText=0x7f070014;
+        public static final int quoteCommentBubble=0x7f07000b;
+        public static final int quoteComments=0x7f07000c;
+        public static final int quoteFavStar=0x7f070009;
+        public static final int quoteFavorites=0x7f07000a;
+        public static final int quoteFootnote=0x7f07000e;
+        public static final int quoteGrade=0x7f070008;
+        public static final int quoteId=0x7f070007;
+        public static final int quoteText=0x7f07000d;
+        public static final int quoteTopBar=0x7f070006;
+        public static final int readersText=0x7f070015;
+        public static final int reportText=0x7f070013;
+        public static final int textView1=0x7f070010;
+        public static final int updateItem=0x7f070018;
+        public static final int warplogo=0x7f070011;
     }
     public static final class layout {
         public static final int browse=0x7f030000;
--- src/net/radspark/android/warpdrive/ViewSingleQuoteActivity.java
+++ src/net/radspark/android/warpdrive/ViewSingleQuoteActivity.java
@@ -11,7 +11,6 @@ import org.jsoup.select.Elements;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
-import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -28,7 +27,7 @@ public class ViewSingleQuoteActivity extends Activity {
 				
 				for(Element div : commentDivs) {
 					String author = div.child(0).child(2).html();
-					String date   = div.child(0).html().replaceAll("<.*?>.*?<\\/.*?>", "");
+					String date   = div.child(0).html().substring(div.child(0).html().length() - 20, div.child(0).html().length() - 1);
 					String text   = Html.fromHtml(div.child(1).html()).toString();
 					
 					comments.add(new QuoteComment(author, date, text));
@@ -38,6 +37,8 @@ public class ViewSingleQuoteActivity extends Activity {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		} else {
+			comments.add(new QuoteComment("", "", "Det här citatet har inte blivit kommenterat ännu."));
 		}
 		
 		return comments;
@@ -49,14 +50,19 @@ public class ViewSingleQuoteActivity extends Activity {
 			
 			Element mainDiv = doc.select("#main").get(0).child(0);
 			
-			String reportDate  = mainDiv.html().substring(10, 29);
-			String reporter    = Html.fromHtml(mainDiv.child(0).html()).toString();
+			String[] lines = mainDiv.html().split("<br />");
 			
-			String publishDate = mainDiv.html().replaceAll("<.*?>.*?<\\/.*?>", "").substring(71, 90);
-			String publisher   = mainDiv.child(2).html();
+			for(int i = 0; i < lines.length; i++) {
+				lines[i] = lines[i].replaceAll("<.*?>", "");
+			}
 			
-			String lastPart = mainDiv.html().replaceAll("<.*?>.*?<\\/.*?>", "").substring(123);
-			String readersStr = lastPart.substring(0, lastPart.indexOf(" "));
+			String reportDate  = lines[0].substring(10, 29);
+			String reporter    = Html.fromHtml(lines[0].substring(lines[0].lastIndexOf(" ") + 1)).toString();
+			
+			String publishDate = lines[1].substring(lines[1].length() - 20, lines[1].length() - 1);
+			String publisher   = lines[1].substring(27, lines[1].indexOf("-") - 1);
+			
+			String readersStr = lines[2].substring(26, lines[2].lastIndexOf(" "));
 			int readers = Integer.parseInt(readersStr);
 			
 			return new QuoteInfo(reportDate, reporter, publishDate, publisher, readers);
